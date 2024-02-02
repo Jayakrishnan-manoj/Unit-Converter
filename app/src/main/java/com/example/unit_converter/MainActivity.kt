a2e8309c627d068137b81687beed3073263ed5d8
jk.jayakrishnan27 <91311543+Jayakrishnan-manoj@users.noreply.github.com>
2024-02-02 19:54:02 +0530

    create:main screen

--- app/src/main/java/com/example/unit_converter/MainActivity.kt
+++ app/src/main/java/com/example/unit_converter/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.unit_converter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.unit_converter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
@@ -30,17 +37,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Unit Converter App", fontSize = 32.sp)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UnitConverterTheme {
-        Greeting("Android")
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text(text = "Enter temperature") },
+            singleLine = true,
+        )
+
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Convert")
+
+        }
+
+        Text(text = "The temperature in celcius:", fontSize = 30.sp)
     }
 }
+
